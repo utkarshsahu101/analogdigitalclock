e01783f877850ca199a3c9954a2dee23a6baa0c0
utkarshsahu101 <42902745+utkarshsahu101@users.noreply.github.com>
2020-12-02 20:11:08 +0530

    Add files via upload

---
+++ alarm.js
@@ -0,0 +1,11 @@
+function setAlarm() {
+    let alarmHourValue = document.getElementById('alarmHourValue');
+    let alarmMinuteValue = document.getElementById('alarmMinuteValue');
+    if(alarmHourValue.value > 12 || alarmHourValue.value == "") {
+        let validHourMessage = document.createElement('h6');
+        validHourMessage.innerHTML = "Enter "
+        if(alarmMinuteValue.value >= 60) {
+            
+        }
+    }
+}
